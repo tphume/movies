4dc4dc2a89dee33ed0bc970e4cb112c5772b64b7
Traphume <traphumedev@gmail.com>
2020-10-08 17:34:33 +0700

    Added fetching trailers to repository api interface

--- lib/src/resources/movie_api_provider.dart
+++ lib/src/resources/movie_api_provider.dart
@@ -3,19 +3,31 @@ import 'package:http/http.dart' show Client;
 import 'dart:convert';
 
 import '../models/item.dart';
+import '../models/trailer.dart';
 
 class MovieApiProvider {
   Client client = Client();
   static const String apiKey = String.fromEnvironment('TMDB_KEY');
+  static const String baseUrl = 'http://api.themoviedb.org/3/movie';
 
   Future<Item> fetchMovieList() async {
-    final response = await client
-        .get('http://api.themoviedb.org/3/movie/popular?api_key=$apiKey');
+    final response = await client.get('$baseUrl/popular?api_key=$apiKey');
 
     if (response.statusCode == 200) {
       return Item.fromJson(json.decode(response.body));
     } else {
       throw Exception('Failed to load post with api key $apiKey');
+    }
+  }
+
+  Future<Trailer> fetchTrailer(int movieId) async {
+    final response =
+        await client.get('$baseUrl/$movieId/videos?api_key=$apiKey');
+
+    if (response.statusCode == 200) {
+      return Trailer.fromJson(json.decode(response.body));
+    } else {
+      throw Exception('Failed to load trailers');
     }
   }
 }
--- lib/src/resources/repository.dart
+++ lib/src/resources/repository.dart
@@ -1,9 +1,13 @@
 import 'dart:async';
 import 'movie_api_provider.dart';
 import '../models/item.dart';
+import '../models/trailer.dart';
 
 class Repository {
   final MovieApiProvider movieApiProvider = MovieApiProvider();
 
   Future<Item> fetchAllMovies() async => movieApiProvider.fetchMovieList();
+
+  Future<Trailer> fetchTrailer(int movieId) async =>
+      movieApiProvider.fetchTrailer(movieId);
 }
