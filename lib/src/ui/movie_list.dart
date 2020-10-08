ebb375172d86855a568520d94d195f737a7443fe
Traphume <traphumedev@gmail.com>
2020-10-08 15:30:02 +0700

    Updated ui to stateful widget to fix memory leak and avoid side effect in build method

--- lib/src/ui/movie_list.dart
+++ lib/src/ui/movie_list.dart
@@ -3,11 +3,26 @@ import 'package:flutter/material.dart';
 import '../models/item.dart';
 import '../blocs/movies.dart';
 
-class MovieList extends StatelessWidget {
+class MovieList extends StatefulWidget {
+  @override
+  _MovieListState createState() => _MovieListState();
+}
+
+class _MovieListState extends State<MovieList> {
+  @override
+  void initState() {
+    super.initState();
+    bloc.fetchAllMovies();
+  }
+
+  @override
+  void dispose() {
+    bloc.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    bloc.fetchAllMovies();
-
     return Scaffold(
       appBar: AppBar(
         title: Text('Popular Movies'),
